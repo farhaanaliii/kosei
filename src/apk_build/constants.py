17cf5faa97e634bf638fddbacd434a8e5895770d
Farhan Ali <officialofun@gmail.com>
2026-08-15 16:47:41 +0500

    fix(constants): correct data path in DATA constant

--- src/apk_build/constants.py
+++ src/apk_build/constants.py
@@ -1,8 +1,7 @@
 from importlib.resources import files
-from pathlib import Path
 
 
-DATA = files("apk_build") / data
+DATA = files("apk_build") / "data"
 DALVIK_VM = "/apex/com.android.art/bin/dalvikvm"
 TEMPLATES = files("apk_build") / "templates"
 
